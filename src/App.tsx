98abb85a7e23fad945b49ddac28db7d2fffa6407
kimywann <kimywan10@gmail.com>
2025-05-08 23:08:04 +0900

    feat: App 컴포넌트 레이아웃 구성 및 주요 컴포넌트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import './App.css'
+import "./App.css";
+
+import Header from "./components/Header";
+import FileUploadHandler from "./components/FileUploadHandler";
+
+import FileTree from "./components/FileTree";
+import EditorTabs from "./components/EditorTabs";
+import Editor from "./components/Editor";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div className="app-container">
+      <Header />
+      <FileUploadHandler />
+      <div className="main-content">
+        <div className="file-tree">
+          <FileTree />
+        </div>
+        <div className="editor-section">
+          <EditorTabs />
+          <Editor />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
