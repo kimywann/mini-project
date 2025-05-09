cb8d12de74271153c7aa8fe8738a4158be813538
kimywann <kimywan10@gmail.com>
2025-05-09 23:05:18 +0900

    feat: 파일 트리 UI 구성 및 디렉토리 열기/닫기, 탭 열기, 파일 선택 처리 구현

--- src/components/FileTree.tsx
+++ src/components/FileTree.tsx
@@ -1,24 +1,88 @@
 import styled from "@emotion/styled";
+import { useState } from "react";
+import type { TreeNode } from "../utils/treeBuilder";
+import { useTab } from "../contexts/useTab";
+
+interface FileTreeProps {
+  tree: TreeNode[];
+  onFileSelect?: (filePath: string, fileContent: string) => void;
+}
 
 const SidebarContainer = styled.aside`
   width: 240px;
   height: 100vh;
-  background-color: rgb(255, 255, 255);
+  background-color: white;
   border-right: 1px solid #e4e4e4;
-  border-top: 1px solid #e4e4e4;
   padding: 1rem;
-  box-sizing: border-box;
   overflow-y: auto;
 `;
 
-const FileTree = () => {
-  return (
-    <>
-      <SidebarContainer>
-        <div>FileTree</div>
-      </SidebarContainer>
-    </>
-  );
+const NodeWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  padding-left: 1rem;
+  margin-bottom: 6px;
+  cursor: pointer;
+`;
+
+const Arrow = styled.span`
+  display: inline-block;
+  width: 1.5rem;
+`;
+
+const Node = styled.div<{ isDirectory: boolean }>`
+  font-weight: ${({ isDirectory }) => (isDirectory ? "bold" : "normal")};
+`;
+
+const FileTree = ({ tree, onFileSelect }: FileTreeProps) => {
+  const [expanded, setExpanded] = useState<Record<string, boolean>>({});
+  const { openTab } = useTab(); // 👈 탭 열기 함수 사용
+
+  const toggle = (path: string) => {
+    setExpanded((prev) => ({ ...prev, [path]: !prev[path] }));
+  };
+
+  const handleFileClick = (node: TreeNode, nodePath: string) => {
+    if (!node.isDirectory) {
+      openTab({ name: node.name, path: nodePath });
+      if (onFileSelect) {
+        // node.content가 없으면 빈 문자열로 전달
+        onFileSelect(nodePath, node.content || "");
+      }
+    }
+  };
+
+  const renderTree = (nodes: TreeNode[], path = "") =>
+    nodes.map((node) => {
+      const nodePath = `${path}/${node.name}`;
+      const isExpanded = expanded[nodePath];
+
+      return (
+        <div key={nodePath}>
+          <NodeWrapper
+            onClick={() => {
+              if (node.isDirectory) {
+                toggle(nodePath);
+              } else {
+                handleFileClick(node, nodePath);
+              }
+            }}
+          >
+            {node.isDirectory && <Arrow>{isExpanded ? "▼" : "▶"}</Arrow>}
+            {!node.isDirectory && <Arrow />} {/* 빈 자리 맞추기 */}
+            <Node isDirectory={node.isDirectory}>{node.name}</Node>
+          </NodeWrapper>
+
+          {node.isDirectory && isExpanded && node.children && (
+            <div style={{ paddingLeft: "1rem" }}>
+              {renderTree(node.children, nodePath)}
+            </div>
+          )}
+        </div>
+      );
+    });
+
+  return <SidebarContainer>{renderTree(tree)}</SidebarContainer>;
 };
 
 export default FileTree;
